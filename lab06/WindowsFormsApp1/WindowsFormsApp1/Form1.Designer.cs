2bb0bbe30b8184cda0b1ef0ed3002e96c8bab6c4
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2021-10-22 15:05:28 +0700

    lab6
    update

--- lab06/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ lab06/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,13 +29,74 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lvCatelory = new System.Windows.Forms.ListView();
+            this.chID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(449, 72);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(115, 33);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Lấy danh sách";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lvCatelory
+            // 
+            this.lvCatelory.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chID,
+            this.chName,
+            this.chType});
+            this.lvCatelory.FullRowSelect = true;
+            this.lvCatelory.HideSelection = false;
+            this.lvCatelory.Location = new System.Drawing.Point(3, 116);
+            this.lvCatelory.MultiSelect = false;
+            this.lvCatelory.Name = "lvCatelory";
+            this.lvCatelory.Size = new System.Drawing.Size(795, 331);
+            this.lvCatelory.TabIndex = 1;
+            this.lvCatelory.UseCompatibleStateImageBehavior = false;
+            this.lvCatelory.View = System.Windows.Forms.View.Details;
+            // 
+            // chID
+            // 
+            this.chID.Text = "Mã loại";
+            this.chID.Width = 150;
+            // 
+            // chName
+            // 
+            this.chName.Text = "Tên loại món ăn";
+            this.chName.Width = 350;
+            // 
+            // chType
+            // 
+            this.chType.Text = "Loại";
+            this.chType.Width = 292;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvCatelory);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListView lvCatelory;
+        private System.Windows.Forms.ColumnHeader chID;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chType;
     }
 }
 
--- lab06/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ lab06/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,27 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            // tạo chuỗi  kết nối tới cơ sở dữ liệu RestaurantManagerment
+            string connectionString = "server =.;database= RestaurantManagement; Intergated Security = true;";
+
+            // Tạo đối tượng kết nối 
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            // Tạo đối tượng thhực thi lệnh 
+            string query = "SELECT ID, Name, Type FROM Category";
+
+            // Mở kết nối đến cơ sở dữ liệu 
+            sqlConnection.Open();
+
+            // GỌi hàm hiển thị dữ liệu lên màn hình 
+            this.DisplayCatelory(SqlDataReader);
+
+            //Đóng kết nối
+            sqlConnection.Close();
         }
     }
 }
