dc4fa3521cc104d67569b3f8bae4787a3c8c8c7d
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2021-10-24 20:49:18 +0700

    C#
    update

--- lab06/WindowsFormsApp1/WindowsFormsApp1/TableForm.Designer.cs
+++ lab06/WindowsFormsApp1/WindowsFormsApp1/TableForm.Designer.cs
@@ -29,12 +29,32 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCheckBills = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCheckBills
+            // 
+            this.btnCheckBills.Location = new System.Drawing.Point(416, 13);
+            this.btnCheckBills.Name = "btnCheckBills";
+            this.btnCheckBills.Size = new System.Drawing.Size(125, 28);
+            this.btnCheckBills.TabIndex = 0;
+            this.btnCheckBills.Text = "Xem hóa đơn";
+            this.btnCheckBills.UseVisualStyleBackColor = true;
+            // 
+            // TableForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheckBills);
+            this.Name = "TableForm";
             this.Text = "TableForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCheckBills;
     }
 }
