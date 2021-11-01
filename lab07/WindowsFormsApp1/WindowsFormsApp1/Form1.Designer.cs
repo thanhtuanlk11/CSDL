ceafa833ce41e1217855cad2b292ca71ce2db249
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2021-11-01 09:27:25 +0700

    C#

--- lab07/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ lab07/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,13 +29,110 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbbCatelory = new System.Windows.Forms.ComboBox();
+            this.dgvFoodList = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.lblCatName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFoodList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn nhóm món ăn :";
+            // 
+            // cbbCatelory
+            // 
+            this.cbbCatelory.FormattingEnabled = true;
+            this.cbbCatelory.Location = new System.Drawing.Point(240, 13);
+            this.cbbCatelory.Name = "cbbCatelory";
+            this.cbbCatelory.Size = new System.Drawing.Size(207, 24);
+            this.cbbCatelory.TabIndex = 1;
+            // 
+            // dgvFoodList
+            // 
+            this.dgvFoodList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFoodList.Location = new System.Drawing.Point(5, 57);
+            this.dgvFoodList.MultiSelect = false;
+            this.dgvFoodList.Name = "dgvFoodList";
+            this.dgvFoodList.RowHeadersWidth = 51;
+            this.dgvFoodList.RowTemplate.Height = 24;
+            this.dgvFoodList.Size = new System.Drawing.Size(792, 327);
+            this.dgvFoodList.TabIndex = 2;
+         
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 401);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Có tất cả ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 401);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "món ăn thuộc nhóm";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(131, 400);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(20, 17);
+            this.lblQuantity.TabIndex = 5;
+            this.lblQuantity.Text = "...";
+            // 
+            // lblCatName
+            // 
+            this.lblCatName.AutoSize = true;
+            this.lblCatName.Location = new System.Drawing.Point(390, 401);
+            this.lblCatName.Name = "lblCatName";
+            this.lblCatName.Size = new System.Drawing.Size(20, 17);
+            this.lblCatName.TabIndex = 6;
+            this.lblCatName.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCatName);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvFoodList);
+            this.Controls.Add(this.cbbCatelory);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFoodList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbCatelory;
+        private System.Windows.Forms.DataGridView dgvFoodList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Label lblCatName;
     }
 }
 
--- lab07/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ lab07/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,34 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+        private void LoadCatelory()
+        {
+            string connectionString = @"Data Source=DESKTOP-RDFL65K\SQLEXPRESS;Initial Catalog=RestaurantManagement;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select ID,Name from Category";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            conn.Open();
+
+            adapter.Fill(dt);
+            conn.Close();
+            // giải phóng bộ nhớ
+            conn.Dispose();
+
+            cbbCatelory.DataSource = dt;
+            //Hiển thị tên nhóm sản phẩm
+            cbbCatelory.DisplayMember = "Name";
+            // Nhưng khi lấy giá trị là lấy ID của nhóm 
+            cbbCatelory.ValueMember = "ID";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.LoadCatelory();
+        }
+
     }
 }
