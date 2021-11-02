a1399312dc75181a93478b438d06309fc18a5036
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2021-11-02 08:39:53 +0700

    C#

--- lab07/WindowsFormsApp1/WindowsFormsApp1/fOODInfoForm.Designer.cs
+++ lab07/WindowsFormsApp1/WindowsFormsApp1/fOODInfoForm.Designer.cs
@@ -29,12 +29,205 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFoodID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtUnit = new System.Windows.Forms.TextBox();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.cbbCatName = new System.Windows.Forms.ComboBox();
+            this.nudPrice = new System.Windows.Forms.DomainUpDown();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FoodID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Unit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CategoryName";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 312);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Notes";
+            // 
+            // txtFoodID
+            // 
+            this.txtFoodID.Location = new System.Drawing.Point(166, 23);
+            this.txtFoodID.Name = "txtFoodID";
+            this.txtFoodID.ReadOnly = true;
+            this.txtFoodID.Size = new System.Drawing.Size(198, 22);
+            this.txtFoodID.TabIndex = 6;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(166, 75);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(586, 22);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtUnit
+            // 
+            this.txtUnit.Location = new System.Drawing.Point(166, 138);
+            this.txtUnit.Name = "txtUnit";
+            this.txtUnit.Size = new System.Drawing.Size(586, 22);
+            this.txtUnit.TabIndex = 8;
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Location = new System.Drawing.Point(166, 307);
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(586, 22);
+            this.txtNotes.TabIndex = 11;
+            // 
+            // cbbCatName
+            // 
+            this.cbbCatName.FormattingEnabled = true;
+            this.cbbCatName.Location = new System.Drawing.Point(166, 191);
+            this.cbbCatName.Name = "cbbCatName";
+            this.cbbCatName.Size = new System.Drawing.Size(476, 24);
+            this.cbbCatName.TabIndex = 12;
+            // 
+            // nudPrice
+            // 
+            this.nudPrice.Location = new System.Drawing.Point(166, 253);
+            this.nudPrice.Name = "nudPrice";
+            this.nudPrice.Size = new System.Drawing.Size(586, 22);
+            this.nudPrice.TabIndex = 13;
+            this.nudPrice.Text = "0";
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(667, 191);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(88, 26);
+            this.btnAddNew.TabIndex = 14;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(222, 373);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(79, 31);
+            this.btnAdd.TabIndex = 15;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(393, 372);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(82, 32);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(580, 372);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(82, 32);
+            this.btnCancel.TabIndex = 17;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // fOODInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.nudPrice);
+            this.Controls.Add(this.cbbCatName);
+            this.Controls.Add(this.txtNotes);
+            this.Controls.Add(this.txtUnit);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtFoodID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "fOODInfoForm";
             this.Text = "fOODInfoForm";
+            this.Load += new System.EventHandler(this.fOODInfoForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFoodID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtUnit;
+        private System.Windows.Forms.TextBox txtNotes;
+        private System.Windows.Forms.ComboBox cbbCatName;
+        private System.Windows.Forms.DomainUpDown nudPrice;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
--- lab07/WindowsFormsApp1/WindowsFormsApp1/fOODInfoForm.cs
+++ lab07/WindowsFormsApp1/WindowsFormsApp1/fOODInfoForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,44 @@ namespace WindowsFormsApp1
         public fOODInfoForm()
         {
             InitializeComponent();
+        }
+
+        private void fOODInfoForm_Load(object sender, EventArgs e)
+        {
+            this.InitValues();
+        }
+        private void InitValues()
+        {
+            string connectionString = @"Data Source=DESKTOP-RDFL65K\SQLEXPRESS;Initial Catalog=RestaurantManagement;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select ID,Name from Category";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            conn.Open();
+            // Lấy dữ liệu từ csdl đưa vào datatable 
+            adapter.Fill(ds, "Category");
+
+            // Hiển thị nhóm món ăn 
+            cbbCatName.DataSource = ds.Tables["Category"];
+            cbbCatName.DisplayMember = "Name";
+            cbbCatName.ValueMember = "ID";
+            // đóng kết nôi và giải phóng bộ nhwos 
+
+            conn.Dispose();
+            conn.Close();
+        }
+        private void ResetText()
+        {
+            txtFoodID.ResetText();
+            txtName.ResetText();
+            txtNotes.ResetText();
+            txtUnit.ResetText();
+            cbbCatName.ResetText();
+            nudPrice.ResetText();
         }
     }
 }
