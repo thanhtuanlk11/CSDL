3184b70c05933223cc54a8ee22f3f1c50cc84272
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2021-11-22 19:53:20 +0700

    Create Food.cs

---
+++ lab09/WindowsFormsApp1/WindowsFormsApp1/Models/Food.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+    public class Food
+    {
+        public int  Id { get; set; }
+        public string  Name { get; set; }
+        public string Unit { get; set; }
+        public int FoodCategoryId { get; set; }
+        public int Price { get; set; }
+        public string Notes { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
