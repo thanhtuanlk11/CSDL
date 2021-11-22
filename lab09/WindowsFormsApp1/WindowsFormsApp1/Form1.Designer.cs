0c929a286885077ff6ae1b8cc2250255b4bcf8d2
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2021-11-22 19:41:30 +0700

    C#

--- lab09/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ lab09/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class tvwCategory
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,129 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnReloadCategory = new System.Windows.Forms.Button();
+            this.btnAddCategory = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddFood = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReloadFood = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.lvFood = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnReloadCategory
+            // 
+            this.btnReloadCategory.Location = new System.Drawing.Point(111, 13);
+            this.btnReloadCategory.Name = "btnReloadCategory";
+            this.btnReloadCategory.Size = new System.Drawing.Size(33, 32);
+            this.btnReloadCategory.TabIndex = 0;
+            this.btnReloadCategory.Text = "R";
+            this.btnReloadCategory.UseVisualStyleBackColor = true;
+            // 
+            // btnAddCategory
+            // 
+            this.btnAddCategory.Location = new System.Drawing.Point(150, 13);
+            this.btnAddCategory.Name = "btnAddCategory";
+            this.btnAddCategory.Size = new System.Drawing.Size(35, 32);
+            this.btnAddCategory.TabIndex = 1;
+            this.btnAddCategory.Text = "+";
+            this.btnAddCategory.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Danh mục";
+            // 
+            // btnAddFood
+            // 
+            this.btnAddFood.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddFood.Location = new System.Drawing.Point(730, 13);
+            this.btnAddFood.Name = "btnAddFood";
+            this.btnAddFood.Size = new System.Drawing.Size(35, 32);
+            this.btnAddFood.TabIndex = 3;
+            this.btnAddFood.Text = "+";
+            this.btnAddFood.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(689, 13);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(35, 32);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "-";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnReloadFood
+            // 
+            this.btnReloadFood.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReloadFood.Location = new System.Drawing.Point(648, 13);
+            this.btnReloadFood.Name = "btnReloadFood";
+            this.btnReloadFood.Size = new System.Drawing.Size(35, 32);
+            this.btnReloadFood.TabIndex = 5;
+            this.btnReloadFood.Text = "R";
+            this.btnReloadFood.UseVisualStyleBackColor = true;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.Location = new System.Drawing.Point(12, 51);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(218, 394);
+            this.treeView1.TabIndex = 6;
+            // 
+            // lvFood
+            // 
+            this.lvFood.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvFood.FullRowSelect = true;
+            this.lvFood.GridLines = true;
+            this.lvFood.HideSelection = false;
+            this.lvFood.Location = new System.Drawing.Point(236, 51);
+            this.lvFood.MultiSelect = false;
+            this.lvFood.Name = "lvFood";
+            this.lvFood.Size = new System.Drawing.Size(552, 394);
+            this.lvFood.TabIndex = 7;
+            this.lvFood.UseCompatibleStateImageBehavior = false;
+            this.lvFood.View = System.Windows.Forms.View.Details;
+            // 
+            // tvwCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lvFood);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnReloadFood);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddFood);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddCategory);
+            this.Controls.Add(this.btnReloadCategory);
+            this.Name = "tvwCategory";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý nhà hàng ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnReloadCategory;
+        private System.Windows.Forms.Button btnAddCategory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddFood;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReloadFood;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView lvFood;
     }
 }
 
