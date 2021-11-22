245237ef778b0bcd7eb3bcc5f04bbe58997af28d
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2021-11-22 20:21:12 +0700

    C#

---
+++ lab09/WindowsFormsApp1/WindowsFormsApp1/Models/FoodModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+    public class FoodModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public string CategoryName { get; set; }
+        public int Price { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
