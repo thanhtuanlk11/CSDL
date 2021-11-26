d863b1bdc5b86c01dfc3d15be94f700a1428574d
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2021-11-26 08:56:12 +0700

    Update UpdateFoodForm.cs

--- lab09/WindowsFormsApp1/WindowsFormsApp1/UpdateFoodForm.cs
+++ lab09/WindowsFormsApp1/WindowsFormsApp1/UpdateFoodForm.cs
@@ -7,14 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Models;
 
 namespace WindowsFormsApp1
 {
     public partial class UpdateFoodForm : Form
     {
-        public UpdateFoodForm()
+        private RestaurantContext _dbContext;
+        private int _foodID;
+        public UpdateFoodForm(int ? foodId = null)
         {
             InitializeComponent();
+            _dbContext = new RestaurantContext();
+            _foodID = foodId ?? 0;
         }
 
         private void UpdateFoodForm_Load(object sender, EventArgs e)
